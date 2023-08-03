b26b2dae31b2813bf01fc0a25f5efa4bfb6df3ee
AdrianRamirez7 <adrian.ramirez45679@gmail.com>
2023-08-03 12:55:52 -0700

    inital

--- kiwanis.js
+++ kiwanis.js
@@ -1,4 +1,25 @@
-const kiwanis = () => {
-    document.getElementById
-}
+let navBar = document.getElementById("nav-text");
+let navBarTwo = document.getElementById("nav-text2");
+let navBarThree = document.getElementById("nav-text3");
+let navBarFour = document.getElementById("nav-text4");
 
+function adjustWidth() {
+    navBar.style.fontSize = '35px';
+    navBar.style.color = 'goldenrod';
+    navBarTwo.style.fontSize = '35px';
+    navBarTwo.style.color = 'goldenrod';
+};
+
+
+navBar.addEventListener('mouseover', adjustWidth);
+navBarTwo.addEventListener('mouseover', adjustWidth);
+
+function resetWidth() {
+    navBar.style.fontSize = '25px';
+    navBar.style.color = 'white';
+    navBarTwo.style.fontSize = '25px';
+    navBarTwo.style.color = 'white';
+};
+
+navBar.addEventListener('mouseout', resetWidth);
+navBarTwo.addEventListener('mouseout', resetWidth);
